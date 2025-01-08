e176f0839192da467a1b1696c3c5d30e7f3e345b
BaoOnHead <jensenzhang@lilith.com>
2025-01-08 16:07:51 +0800

    feat: rm readme

---
+++ change_git_remote.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# 定义颜色和样式的ANSI转义序列
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BOLD='\033[1m'
+NC='\033[0m' # No Color
+
+OKTAG="${BOLD}${GREEN}[OK]${NC}"
+ERRTAG="${BOLD}${RED}[ER]${NC}"
+
+# echo 现在的git remote
+echo -e "${BOLD}Current git remote: ${NC}"
+git remote -v
+
+# 询问是否要修改git remote
+read -p "Do you want to change the git remote? (y/n) " answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+    # 修改git remote
+    read -p "Please input the new git remote: " new_remote
+    git remote set-url origin $new_remote
+    echo -e "${OKTAG} Change git remote to $new_remote"
+else
+    echo -e "${OKTAG} Keep the current git remote"
+fi
+
+# echo 修改后的git remote
+echo -e "${BOLD}Current git remote: ${NC}"
+git remote -v
+
+# 远端如果是空分支，将本地的分支推送到远端
+read -p "Do you want to push the local branch to the remote branch? (y/n) " answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+    # 推送本地分支到远端
+    git push -u origin main
+    echo -e "${OKTAG} Push the local branch to the remote branch"
+else
+    echo -e "${OKTAG} Skip pushing the local branch to the remote branch"
+fi
--- start.sh
+++ start.sh
@@ -1,1 +1,12 @@
-cd scripts && ./rest.sh app
+#!/bin/bash
+
+service_name=$1
+
+# 如果service_name没有输入，提示使用app为默认值
+if [ -z "$service_name" ]; then
+    echo -e "${BOLD}service_name is empty, use 'app' as default value.${NC}"
+    service_name="app"
+fi
+
+cd scripts && ./rest.sh $service_name
+
